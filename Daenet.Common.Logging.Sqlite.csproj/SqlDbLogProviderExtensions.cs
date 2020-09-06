3a21f43a3c6b28ac47c501ebf3ebcc79cfae8a6a
Dima Stadub <dstadub@ezlinks.com>
2020-09-06 12:42:56 +0300

    Create sqlite logger  implementation

---
+++ Daenet.Common.Logging.Sqlite.csproj/SqlDbLogProviderExtensions.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Lesofto.Common.Logging.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Daenet.Common.Logging.Sql
+{
+    /// <summary>
+    /// Extensions for working work ILogger and other interfaces.
+    /// </summary>
+    public static class SqliteLogProviderExtensions
+    {
+
+
+        /// <summary>
+        /// Adds a sql logger named 'SqlLogger' to the factory.
+        /// </summary>
+        /// <param name="builder">The <see cref="ILoggingBuilder"/> to use.</param>
+        public static ILoggingBuilder AddSqliteLogger(this ILoggingBuilder builder)
+        {
+            builder.Services.AddSingleton<ISqlBatchLogTask, SqliteBatchLogTask>();
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, SqlLogProvider>());
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a sql logger named 'SqlLogger' to the factory.
+        /// </summary>
+        /// <param name="builder">The <see cref="ILoggingBuilder"/> to use.</param>
+        /// <param name="configure"></param>
+        public static ILoggingBuilder AddSqliteLogger(this ILoggingBuilder builder, Action<SqlLoggerSettings> configure)
+        {
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            builder.AddSqliteLogger();
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, SqlLogProvider>());
+
+            builder.Services.Configure(configure);
+
+            return builder;
+        }
+
+
+
+        /// <summary>
+        /// Adds Sql Logger to LoggerFactory
+        /// </summary>
+        /// <param name="loggerFactory">LoggerFactory Instance</param>
+        /// <param name="settings">Sql Logger Settings</param>
+        /// <param name="filter">If specified it will override all defined switches.</param>  
+        /// <returns></returns>        
+        public static ILoggerFactory AddSqlServerLogger(this ILoggerFactory loggerFactory,
+          ISqlLoggerSettings settings)
+        {
+            var logger = new SqliteBatchLogTask(settings);
+            loggerFactory.AddProvider(new SqlLogProvider(logger, settings));
+
+            return loggerFactory;
+        }
+
+        /// <summary>
+        /// Adds a SQL Logger to the Logger factory.
+        /// </summary>
+        /// <param name="loggerFactory">The Logger factory instance.</param>
+        /// <param name="config">The .NET Core Configuration for the logger section.</param>
+        /// <returns></returns>
+        public static ILoggerFactory AddSqlServerLogger(this ILoggerFactory loggerFactory, IConfiguration config)
+        {
+            var settings = config.GetSqlLoggerSettings();
+            var logger = new SqliteBatchLogTask(settings);
+            loggerFactory.AddProvider(new SqlLogProvider(logger, settings));
+
+            return loggerFactory;
+        }
+
+
+
+
+    }
+}
---
+++ Daenet.Common.Logging.Sqlite.csproj/SqliteBatchLogTask.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Daenet.Common.Logging.Sql;
+using SQLite;
+
+namespace Lesofto.Common.Logging.Sqlite
+{
+    public class SqliteBatchLogTask: SqlBatchLogTask
+    {
+        private string _connectionString;
+        private SQLiteOpenFlags _openFlags;
+
+        public SqliteBatchLogTask(ISqlLoggerSettings settings) : base(settings)
+        {
+            _connectionString = settings.ConnectionString;
+            _openFlags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.Create;
+        }
+
+        private async Task Write(Event[] listToWrite)
+        {
+            var connection = new SQLiteAsyncConnection(_connectionString, _openFlags);
+            
+            await connection.CreateTableAsync<Event>();
+            var table = connection.Table<Event>();
+
+            await connection.InsertAllAsync(listToWrite);
+
+            
+        }
+
+        protected override void WriteToDb(Event[] listToWrite)
+        {
+            // Don't do anything if list is empty.
+            if (CurrentList.Count == 0)
+                return;
+
+            try
+            {
+                
+            }
+            catch (InvalidOperationException invalidEx)
+            {
+                if (invalidEx.Message == "The given ColumnMapping does not match up with any column in the source or destination.")
+                {
+
+                    var columns = String.Join(",", DbColumnMapping.Select(d => d.DestinationColumn));
+                    HandleError(new Exception($"Missing/Invalid table columns. Required columns: {columns}", invalidEx));
+                }
+                else
+                    HandleError(invalidEx);
+            }
+            catch (Exception e)
+            {
+                HandleError(e);
+            }
+        }
+    }
+}
