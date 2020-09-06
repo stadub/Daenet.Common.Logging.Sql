5236aa938ac42c3f1d96bcf60136980a83875562
Dima Stadub <dstadub@ezlinks.com>
2020-09-06 12:42:29 +0300

    Add sql Server logger implementation

---
+++ Daenet.Common.Logging.SqlServer.csproj/CustomDataReader.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Daenet.Common.Logging.Sql
+{
+    class CustomDataReader : IDataReader
+    {
+        private IEnumerator<Event> _enumerator = null;
+
+        private readonly IList<Event> _data;
+
+        public CustomDataReader(IList<Event> data)
+        {
+            this._enumerator = data.GetEnumerator();
+            this._data = data;
+        }
+
+        #region Not needed Properties
+
+        public object this[int i] => throw new NotImplementedException();
+
+        public object this[string name] => throw new NotImplementedException();
+
+        public int Depth => throw new NotImplementedException();
+
+        public bool IsClosed => throw new NotImplementedException();
+
+        public int RecordsAffected => throw new NotImplementedException();
+
+        #endregion
+
+        public int FieldCount
+        {
+            get
+            {
+                if (_enumerator.Current != null)
+                {
+                    return _enumerator.Current.Count;
+                }
+                else
+                    return _data.First().Count;
+            }
+        }
+
+        public void Close()
+        {
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            _enumerator.Dispose();
+        }
+
+        public bool Read()
+        {
+            return _enumerator.MoveNext();
+        }
+
+        public object GetValue(int i)
+        {
+            return _enumerator.Current[i];
+        }
+
+        public int GetOrdinal(string name)
+        {
+            return Int32.Parse(name);
+        }
+
+        #region Not needed Methods
+
+        bool IDataRecord.GetBoolean(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        byte IDataRecord.GetByte(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        char IDataRecord.GetChar(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        long IDataRecord.GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader GetData(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IDataRecord.GetDataTypeName(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        DateTime IDataRecord.GetDateTime(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        decimal IDataRecord.GetDecimal(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        double IDataRecord.GetDouble(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        Type IDataRecord.GetFieldType(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        float IDataRecord.GetFloat(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        Guid IDataRecord.GetGuid(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        short IDataRecord.GetInt16(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IDataRecord.GetInt32(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        long IDataRecord.GetInt64(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IDataRecord.GetName(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        DataTable IDataReader.GetSchemaTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        string IDataRecord.GetString(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IDataRecord.GetValues(object[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDataRecord.IsDBNull(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDataReader.NextResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Daenet.Common.Logging.SqlServer.csproj/SqlServerBatchLogTask.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Daenet.Common.Logging.Sql
+{
+    public class SqlServerBatchLogTask: SqlBatchLogTask
+    {
+        private string _connectionString;
+        public SqlServerBatchLogTask(ISqlLoggerSettings settings) : base(settings)
+        {
+            _connectionString = settings.ConnectionString;
+        }
+
+        protected override void WriteToDb(Event[] listToWrite)
+        {
+            // Don't do anything if list is empty.
+            if (CurrentList.Count == 0)
+                return;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    //create object of SqlBulkCopy which help to insert  
+                    using (SqlBulkCopy objbulk = new SqlBulkCopy(con))
+                    {
+                        CustomDataReader customDataReader = new CustomDataReader(listToWrite);
+                        objbulk.DestinationTableName = TableName;
+
+                        foreach (var mapping in DbColumnMapping)
+                        {
+                            objbulk.ColumnMappings.Add(mapping);
+                        }
+
+                        con.Open();
+
+                        if (BatchSize <= 1) // use sync method if BatchSize is < 1
+                            objbulk.WriteToServer(customDataReader);
+                        else // use async methodd
+                            objbulk.WriteToServerAsync(customDataReader);
+                    }
+                }
+            }
+            catch (InvalidOperationException invalidEx)
+            {
+                if (invalidEx.Message == "The given ColumnMapping does not match up with any column in the source or destination.")
+                {
+
+                    var columns = String.Join(",", DbColumnMapping.Select(d => d.DestinationColumn));
+                    HandleError(new Exception($"Missing/Invalid table columns. Required columns: {columns}", invalidEx));
+                }
+                else
+                    HandleError(invalidEx);
+            }
+            catch (Exception e)
+            {
+                HandleError(e);
+            }
+        }
+    }
+}
